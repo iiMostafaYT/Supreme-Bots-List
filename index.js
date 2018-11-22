46bbcc9b328a6936856e597ded475da0612ff1cd
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-22 17:10:15 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '.'; // Put the Prefix You want, Also there must be 2 spaces between the const prefix and client.login 
+
+client.login('TOKEN'); // Put Your Token Here
