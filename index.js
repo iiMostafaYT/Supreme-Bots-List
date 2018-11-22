87989642bb1a7360a914011b46eafef6b4c733cd
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-22 19:39:29 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,55 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'; // Put the Prefix You want, Also there must be 2 spaces between the const prefix and client.login 
+const prefix = '$'; // Put the Prefix You want, Also there must be 2 spaces between the const prefix and client.login 
 
-client.login('TOKEN'); // Put Your Token Here
+client.on('message', async message => {
+  if(message.content.startsWith("$تقديم")) {
+    await message.channel.send("** ❓  حسنا, قم بكتابة ايدي البوت**").then(e => {
+    let filter = m => m.author.id === message.author.id
+    let lan = '';
+    let md = '';
+    let br = '';
+    let chaLan = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
+    .then(collected => {
+      lan = collected.first().content
+      collected.first().delete()
+e.delete();
+     message.channel.send('** رائع, الأن قم بكتابة مميزات بوتك ❓**').then(m => {
+let chaMd = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
+.then(co => {
+  md = co.first().content
+        co.first().delete()
+        m.delete();
+message.channel.send('**اخيرا وليس اخرا, قم بكتابة عدد السيرفرات والمستخدمين ❓ **').then(ms => {
+let br = message.channel.awaitMessages(filter, { max: 1, time: 400000, errors: ['time'] })
+.then(col => {
+  br = col.first().content
+        col.first().delete()
+
+ms.delete()
+
+ message.channel.send('جاري التقديم ..').then(b => {
+        setTimeout(() => {
+  b.edit(`**تم التقديم وسيتم الرد فـ اقرب وقت**`)
+        },2000);
+var gg = message.guild.channels.find('name', 'التقديمات')
+if(!gg) return;
+if(gg) {
+gg.send({embed : new Discord.RichEmbed()
+.setDescription(`**  الأيدي :question:  : \n ${lan}\nالمميزات :link: :\n ${md} \nعدد السيرفرات والمستخدمين :question: :\n ${br}  \nتم التقديم بواسطة  : <@${message.author.id}> **`)  
+          .setFooter(`Tesla Server`)
+.setTimestamp()
+});
+}        
+})
+})
+})
+})
+})
+})
+})
+ }
+})
+
+
+client.login(process.env.BOT_TOKEN); // Put Your Token Here
